61fbf0e7da4cd57cf001e73eb72e7c51dbcda9d4
Kyle Seongwoo Jun <njk230@gmail.com>
2023-07-31 21:23:20 +0900

    feat: improve names

--- scripts/scrape-from-xcode.ts
+++ scripts/scrape-from-xcode.ts
@@ -1,21 +1,25 @@
-async function getJSON(platform: string) {
+async function scrapeFromXcode(platform: string) {
     const process = Deno.run({ cmd: ["bash", "-c", `scripts/scrape-from-xcode.sh ${platform}`], stdout: "piped" });
     const output = await process.output().then((o) => new TextDecoder().decode(o));
     const obj = JSON.parse(output);
     return obj as { [key: string]: string };
 }
 
-async function generateJsonFile(platform: { name: string; file: string }) {
-    const json = await getJSON(platform.name);
-
-    // natural sort
-    const collator = new Intl.Collator(undefined, { numeric: true, sensitivity: "base" });
-    const sortedKeys = [...Object.keys(json)].sort((a, b) => collator.compare(a, b));
+const collator = new Intl.Collator(undefined, { numeric: true, sensitivity: "base" });
+function naturalSort(dict: { [key: string]: string }) {
+    const sortedKeys = [...Object.keys(dict)].sort((a, b) => collator.compare(a, b));
     const sortedDict = {} as { [key: string]: string };
-    sortedKeys.forEach((key) => (sortedDict[key] = json[key]));
+    sortedKeys.forEach((key) => (sortedDict[key] = dict[key]));
+    return sortedDict;
+}
 
-    const jsonStr = JSON.stringify(sortedDict, null, 2);
-    await Deno.writeTextFile(platform.file, jsonStr);
+async function generateJsonFile(platform: { name: string; file: string }) {
+    console.log(`Generating ${platform.file}...`);
+    const dict = await scrapeFromXcode(platform.name).then(naturalSort);
+
+    console.log(`Writing ${platform.file}...`);
+    const json = JSON.stringify(dict, null, 2);
+    await Deno.writeTextFile(platform.file, json);
 }
 
 const platforms = [
@@ -24,3 +28,5 @@ const platforms = [
     { name: "AppleTVOS", file: "tvos-device-identifiers.json" },
 ];
 await Promise.all(platforms.map((p) => generateJsonFile(p)));
+
+console.log("Done!");
